5a299bf3aec7d4c0ff2224c7ebe8fd0320f663ba
Josiel Sousa <josiel.psousa@gmail.com>
2022-01-06 14:10:12 -0300

    test: adjust repo unit test

--- app/gateway/db/postgres/accounts/insert_test.go
+++ app/gateway/db/postgres/accounts/insert_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -21,9 +23,10 @@ func TestRepository_Insert(t *testing.T) {
 		acc accounts.Account
 	}
 	tests := []struct {
-		name    string
-		args    func(t *testing.T) args
-		wantErr error
+		name      string
+		args      func(t *testing.T) args
+		checkErr  func(t *testing.T, err error)
+		beforeRun func(t *testing.T, db *pgxpool.Pool)
 	}{
 		{
 			name: "should save account with successfully",
@@ -44,7 +47,83 @@ func TestRepository_Insert(t *testing.T) {
 					ctx: context.Background(),
 				}
 			},
-			wantErr: nil,
+			checkErr: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "should return an postgres error when save new account, because column name is invalid",
+			args: func(t *testing.T) args {
+				secret, err := hash.NewHash("the#$%PassWoRd")
+				require.NoError(t, err)
+
+				newCpf, err := cpf.NewCPF("88350057017")
+				require.NoError(t, err)
+
+				return args{
+					acc: accounts.Account{
+						Name:    "Teste",
+						Balance: 350_00,
+						Secret:  secret,
+						CPF:     newCpf,
+					},
+					ctx: context.Background(),
+				}
+			},
+			beforeRun: func(t *testing.T, db *pgxpool.Pool) {
+				{
+					_, err := db.Exec(context.Background(), "ALTER TABLE accounts RENAME COLUMN name TO username")
+					require.NoError(t, err)
+				}
+			},
+			checkErr: func(t *testing.T, err error) {
+				var pgErr *pgconn.PgError
+				assert.ErrorAs(t, err, &pgErr)
+				assert.Equal(t, `column "name" of relation "accounts" does not exist`, pgErr.Message)
+			},
+		},
+		{
+			name: "should return an error when save a duplicate account",
+			args: func(t *testing.T) args {
+				secret, err := hash.NewHash("the#$%PassWoRd")
+				require.NoError(t, err)
+
+				newCpf, err := cpf.NewCPF("88350057017")
+				require.NoError(t, err)
+
+				return args{
+					acc: accounts.Account{
+						Name:    "Teste",
+						Balance: 350_00,
+						Secret:  secret,
+						CPF:     newCpf,
+					},
+					ctx: context.Background(),
+				}
+			},
+			beforeRun: func(t *testing.T, db *pgxpool.Pool) {
+				{
+					secret, err := hash.NewHash("the#$%PassWoRd")
+					require.NoError(t, err)
+
+					newCpf, err := cpf.NewCPF("88350057017")
+					require.NoError(t, err)
+
+					acc := accounts.Account{
+						Name:    "Teste",
+						Balance: 350_00,
+						Secret:  secret,
+						CPF:     newCpf,
+					}
+
+					err = pgtest.AccountsInsert(db, t, acc)
+					require.NoError(t, err)
+
+				}
+			},
+			checkErr: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, accounts.ErrAccountAlreadyExists)
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -56,10 +135,14 @@ func TestRepository_Insert(t *testing.T) {
 			pgPool := pgtest.NewDB(t)
 			r := NewRepository(pgPool)
 
+			if tt.beforeRun != nil {
+				tt.beforeRun(t, pgPool)
+			}
+
 			args := tt.args(t)
 
 			err := r.Insert(args.ctx, args.acc)
-			assert.ErrorIs(t, err, tt.wantErr)
+			tt.checkErr(t, err)
 		})
 	}
 }

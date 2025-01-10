bd3e4df7ea117fd1a5c481f548fd26df56b73207
Josiel Sousa <josiel.psousa@gmail.com>
2025-01-10 18:23:34 -0300

    refactor: service remove db gorm references

--- service/account.go
+++ service/account.go
@@ -2,14 +2,10 @@ package service
 
 import (
 	"net/http"
-	"time"
-
-	"github.com/google/uuid"
 
 	"github.com/josielsousa/challenge-accounts/helpers/auth"
 	httpHelper "github.com/josielsousa/challenge-accounts/helpers/http"
 	"github.com/josielsousa/challenge-accounts/helpers/validator"
-	"github.com/josielsousa/challenge-accounts/repo/model"
 	"github.com/josielsousa/challenge-accounts/types"
 )
 
@@ -28,19 +24,20 @@ type AccountService struct {
 	authHlp      *auth.Helper
 	httpHlp      *httpHelper.Helper
 	validatorHlp *validator.Helper
-	stgAccount   model.AccountStorage
-	logger       types.APILogProvider
+	// stgAccount   model.AccountStorage
+	logger types.APILogProvider
 }
 
 // NewAccountService - Instância o service com a dependência `log` inicializada.
-func NewAccountService(stgAccount model.AccountStorage, log types.APILogProvider) *AccountService {
-	return &AccountService{
-		logger:       log,
-		stgAccount:   stgAccount,
-		authHlp:      auth.NewHelper(),
-		httpHlp:      httpHelper.NewHelper(),
-		validatorHlp: validator.NewHelper(),
-	}
+func NewAccountService() *AccountService {
+	// return &AccountService{
+	// 	logger:       log,
+	// 	stgAccount:   stgAccount,
+	// 	authHlp:      auth.NewHelper(),
+	// 	httpHlp:      httpHelper.NewHelper(),
+	// 	validatorHlp: validator.NewHelper(),
+	// }
+	return nil
 }
 
 // InsertAccount - Realiza a inserção de uma account conforme os dados do `body` da requisição
@@ -48,46 +45,46 @@ func NewAccountService(stgAccount model.AccountStorage, log types.APILogProvider
 //	200: Sucesso na inserção
 //	422: Erro - Os dados de entrada são válidos porém existe uma `account` para o CPF informado.
 //	500: Erro inesperado durante o processamento da requisição
-func (s *AccountService) InsertAccount(w http.ResponseWriter, req *http.Request) {
-	account := s.validatorHlp.ValidateDataAccount(w, req)
-	if account == nil {
-		return
-	}
-
-	accExist, err := s.stgAccount.GetAccountByCPF(account.Cpf)
-	if err != nil {
-		s.logger.Error("Error on verify account exists: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	exist := accExist != nil && len(accExist.ID) > 0
-	if exist {
-		s.logger.Info("Account exists")
-		s.httpHlp.ThrowError(w, http.StatusUnprocessableEntity, ErrorAccountExist)
-		return
-	}
-
-	createdAt := time.Now()
-	account.CreatedAt = &createdAt
-	account.ID = uuid.New().String()
-
-	secretHash, err := s.authHlp.Hash(account.Secret)
-	if err != nil {
-		s.logger.Error("Error on hash secret account: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	account.Secret = string(secretHash)
-	acc, err := s.stgAccount.Insert(*account)
-	if err != nil {
-		s.logger.Error("Error on insert an account: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	s.httpHlp.ThrowSuccess(w, http.StatusCreated, types.SuccessResponse{Success: true, Data: acc})
+func (s *AccountService) InsertAccount(_ http.ResponseWriter, _ *http.Request) {
+	// account := s.validatorHlp.ValidateDataAccount(w, req)
+	// if account == nil {
+	// 	return
+	// }
+	//
+	// accExist, err := s.stgAccount.GetAccountByCPF(account.Cpf)
+	// if err != nil {
+	// 	s.logger.Error("Error on verify account exists: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// exist := accExist != nil && len(accExist.ID) > 0
+	// if exist {
+	// 	s.logger.Info("Account exists")
+	// 	s.httpHlp.ThrowError(w, http.StatusUnprocessableEntity, ErrorAccountExist)
+	// 	return
+	// }
+	//
+	// createdAt := time.Now()
+	// account.CreatedAt = &createdAt
+	// account.ID = uuid.New().String()
+	//
+	// secretHash, err := s.authHlp.Hash(account.Secret)
+	// if err != nil {
+	// 	s.logger.Error("Error on hash secret account: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// account.Secret = string(secretHash)
+	// acc, err := s.stgAccount.Insert(*account)
+	// if err != nil {
+	// 	s.logger.Error("Error on insert an account: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// s.httpHlp.ThrowSuccess(w, http.StatusCreated, types.SuccessResponse{Success: true, Data: acc})
 }
 
 // GetAllAccounts - Retorna as informações de todas as contas se não existir retorna []
@@ -95,20 +92,20 @@ func (s *AccountService) InsertAccount(w http.ResponseWriter, req *http.Request)
 //	200: Quando existir accounts para serem retornadas
 //	204: Quando não encontrar accounts.
 //	500: Erro inesperado durante o processamento da requisição
-func (s *AccountService) GetAllAccounts(w http.ResponseWriter, req *http.Request) {
-	accounts, err := s.stgAccount.GetAllAccounts()
-	if err != nil {
-		s.logger.Error("Error on get all accounts: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	statusCode := http.StatusOK
-	if len(accounts) <= 0 {
-		statusCode = http.StatusNoContent
-	}
-
-	s.httpHlp.ThrowSuccess(w, statusCode, types.SuccessResponse{Success: true, Data: accounts})
+func (s *AccountService) GetAllAccounts(_ http.ResponseWriter, _ *http.Request) {
+	// accounts, err := s.stgAccount.GetAllAccounts()
+	// if err != nil {
+	// 	s.logger.Error("Error on get all accounts: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// statusCode := http.StatusOK
+	// if len(accounts) <= 0 {
+	// 	statusCode = http.StatusNoContent
+	// }
+	//
+	// s.httpHlp.ThrowSuccess(w, statusCode, types.SuccessResponse{Success: true, Data: accounts})
 }
 
 // GetAccountBalance - Retorna as informações da account, conforme o id informado.
@@ -116,24 +113,24 @@ func (s *AccountService) GetAllAccounts(w http.ResponseWriter, req *http.Request
 //	200: Quando existir account para ser retornada
 //	404: Quando não encontrar a account.
 //	500: Erro inesperado durante o processamento da requisição
-func (s *AccountService) GetAccountBalance(w http.ResponseWriter, req *http.Request) {
+func (s *AccountService) GetAccountBalance(_ http.ResponseWriter, _ *http.Request) {
 	// Recebe os parâmetros da request e seleciona o ID
-	params := s.httpHlp.GetParams(req)
-	id := params["id"]
-
-	account, err := s.stgAccount.GetAccount(id)
-	if err != nil {
-		s.logger.Error("Error on get account: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	success := account != nil && len(account.ID) > 0
-	if !success {
-		s.httpHlp.ThrowError(w, http.StatusNotFound, types.ErroAccountNotFound)
-		return
-	}
-
-	accountBalance := model.AccountBalance{Balance: account.Balance}
-	s.httpHlp.ThrowSuccess(w, http.StatusOK, types.SuccessResponse{Success: success, Data: accountBalance})
+	// params := s.httpHlp.GetParams(req)
+	// id := params["id"]
+	//
+	// account, err := s.stgAccount.GetAccount(id)
+	// if err != nil {
+	// 	s.logger.Error("Error on get account: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// success := account != nil && len(account.ID) > 0
+	// if !success {
+	// 	s.httpHlp.ThrowError(w, http.StatusNotFound, types.ErroAccountNotFound)
+	// 	return
+	// }
+	//
+	// accountBalance := model.AccountBalance{Balance: account.Balance}
+	// s.httpHlp.ThrowSuccess(w, http.StatusOK, types.SuccessResponse{Success: success, Data: accountBalance})
 }
--- service/auth.go
+++ service/auth.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"regexp"
 	"strings"
@@ -16,6 +18,8 @@ import (
 )
 
 // Constantes utilizadas no serviço de autenticação.
+//
+//nolint:gosec
 const (
 	MaxTimeToExpiration        = 5
 	InfoTokenEmpty             = "Token vazio."
@@ -35,18 +39,20 @@ type AuthService struct {
 	httpHlp      *httpHelper.Helper
 	validatorHlp *validator.Helper
 	logger       types.APILogProvider
-	stgAccount   model.AccountStorage
+	// stgAccount   model.AccountStorage
 }
 
 // NewAuthService - Instância o service.
-func NewAuthService(stgAccount model.AccountStorage, log types.APILogProvider) *AuthService {
-	return &AuthService{
-		logger:       log,
-		stgAccount:   stgAccount,
-		authHlp:      auth.NewHelper(),
-		httpHlp:      httpHelper.NewHelper(),
-		validatorHlp: validator.NewHelper(),
-	}
+func NewAuthService() *AuthService {
+	// return &AuthService{
+	// 	logger:       log,
+	// 	stgAccount:   stgAccount,
+	// 	authHlp:      auth.NewHelper(),
+	// 	httpHlp:      httpHelper.NewHelper(),
+	// 	validatorHlp: validator.NewHelper(),
+	// }
+
+	return nil
 }
 
 // Login - Realiza a autenticação do usuário na API.
@@ -56,42 +62,42 @@ func NewAuthService(stgAccount model.AccountStorage, log types.APILogProvider) *
 //	404: Quando não encontrar a account.
 //	500: Erro inesperado durante o processamento da requisição
 func (s *AuthService) Login(w http.ResponseWriter, req *http.Request) {
-	credential := s.validatorHlp.ValidateDataLogin(w, req)
-	if credential == nil {
-		return
-	}
-
-	acc, err := s.stgAccount.GetAccountByCPF(credential.Cpf)
-	if err != nil {
-		s.logger.Error("Error on recovery account by `cpf`: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	successAcc := acc != nil && len(acc.ID) > 0
-	if !successAcc {
-		s.httpHlp.ThrowError(w, http.StatusNotFound, types.ErroAccountNotFound)
-		return
-	}
-
-	// Verifica se o secret informado na autenticação, é o mesmo armazenado na `account`.
-	err = s.authHlp.VerifySecret(acc.Secret, credential.Secret)
-	if err != nil {
-		s.logger.Error("Error on recovery account by `cpf`: ", err)
-		s.httpHlp.ThrowError(w, http.StatusUnauthorized, types.ErrorUnauthorized)
-		return
-	}
-
-	// Tempo de expiração do token
-	expirationTime := time.Now().Add(MaxTimeToExpiration * time.Minute)
-	authToken, err := s.GetToken(acc, jwtKey, expirationTime)
-	if err != nil {
-		s.logger.Error("Error on generate new token by `cpf`: ", err)
-		s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
-		return
-	}
-
-	s.httpHlp.ThrowSuccess(w, http.StatusOK, types.SuccessResponse{Success: true, Data: authToken})
+	// credential := s.validatorHlp.ValidateDataLogin(w, req)
+	// if credential == nil {
+	// 	return
+	// }
+	//
+	// acc, err := s.stgAccount.GetAccountByCPF(credential.Cpf)
+	// if err != nil {
+	// 	s.logger.Error("Error on recovery account by `cpf`: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// successAcc := acc != nil && len(acc.ID) > 0
+	// if !successAcc {
+	// 	s.httpHlp.ThrowError(w, http.StatusNotFound, types.ErroAccountNotFound)
+	// 	return
+	// }
+	//
+	// // Verifica se o secret informado na autenticação, é o mesmo armazenado na `account`.
+	// err = s.authHlp.VerifySecret(acc.Secret, credential.Secret)
+	// if err != nil {
+	// 	s.logger.Error("Error on recovery account by `cpf`: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusUnauthorized, types.ErrorUnauthorized)
+	// 	return
+	// }
+	//
+	// // Tempo de expiração do token
+	// expirationTime := time.Now().Add(MaxTimeToExpiration * time.Minute)
+	// authToken, err := s.GetToken(acc, jwtKey, expirationTime)
+	// if err != nil {
+	// 	s.logger.Error("Error on generate new token by `cpf`: ", err)
+	// 	s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+	// 	return
+	// }
+	//
+	// s.httpHlp.ThrowSuccess(w, http.StatusOK, types.SuccessResponse{Success: true, Data: authToken})
 }
 
 // GetToken - Gera um novo token.
@@ -104,12 +110,14 @@ func (s *AuthService) GetToken(acc *model.Account, jwtKey []byte, expirationTime
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
 	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("on signed token: %w", err)
 	}
 
 	authToken := &types.Auth{Token: tokenString}
+
 	return authToken, nil
 }
 
@@ -123,41 +131,49 @@ func (s *AuthService) ValidateToken(next func(http.ResponseWriter, *http.Request
 	return func(w http.ResponseWriter, req *http.Request) {
 		tokenHeader := req.Header.Get("Access-Token")
 		claims := &types.Claims{}
-		getJwtKey := func(token *jwt.Token) (interface{}, error) {
+
+		getJwtKey := func(_ *jwt.Token) (interface{}, error) {
 			return jwtKey, nil
 		}
 
-		if len(strings.Trim(tokenHeader, " ")) <= 0 || tokenHeader == "null" {
+		if len(strings.Trim(tokenHeader, " ")) == 0 || tokenHeader == "null" {
 			s.logger.Info("Error on parser token claims ")
 			s.httpHlp.ThrowError(w, http.StatusBadRequest, InfoTokenEmpty)
+
 			return
 		}
 
 		jwtToken, err := jwt.ParseWithClaims(tokenHeader, claims, getJwtKey)
 		if err != nil {
 			tokenMalFormed, _ := regexp.MatchString(ErrorTokenMalformed, err.Error())
+
 			tokenSignatureInvalid, _ := regexp.MatchString(ErrorTokenSignatureInvalid, err.Error())
-			if err == jwt.ErrSignatureInvalid || tokenSignatureInvalid || tokenMalFormed {
+			if errors.Is(err, jwt.ErrSignatureInvalid) || tokenSignatureInvalid || tokenMalFormed {
 				s.logger.Error("Error on parser token claims: ", err)
 				s.httpHlp.ThrowError(w, http.StatusUnauthorized, ErrorSignatureKeyInvalid)
+
 				return
 			}
 
 			s.logger.Error("Error unexpected on parser token claims: ", err)
 			s.httpHlp.ThrowError(w, http.StatusInternalServerError, types.ErrorUnexpected)
+
 			return
 		}
 
 		if !jwtToken.Valid {
 			s.logger.Error("Error on parser token claims: ", err)
 			s.httpHlp.ThrowError(w, http.StatusUnauthorized, ErrorTokenInvalid)
+
 			return
 		}
 
-		minutesElapsedLastAuth := time.Unix(claims.ExpiresAt, 0).Sub(time.Now())
+		minutesElapsedLastAuth := time.Until(time.Unix(claims.ExpiresAt, 0))
+
 		if minutesElapsedLastAuth <= 0 {
 			s.logger.Info("Token expired")
 			s.httpHlp.ThrowError(w, http.StatusUnauthorized, InfoTokenExpired)
+
 			return
 		}
 
